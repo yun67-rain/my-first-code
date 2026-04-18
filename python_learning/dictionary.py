42cfe27369eba0cabf3689ecf18f8e214bd066c6
yun67-rain <yuna.0607.0321@gmail.com>
2026-04-18 17:22:42 +0000

    家計簿アプリ

--- python_learning/dictionary.py
+++ python_learning/dictionary.py
@@ -4,37 +4,47 @@ while True:
     while True:
         date=input("日付を入力してください(sで検索,qで終了):")
         if date=="":
-            print("未入力です")
+            print("未入力です\n")
         else:
             break
     if date=="q":
         break
 
     if date=="s":
-        search_date=input("検索したい日付を入力してください:")
+        while True:
+            search_date=input("検索したい日付を入力してください(qで終了):")
 
-        if search_date in expences:
-            data=expences[search_date]
-            print(f"【検索結果】{search_date}は[{data['category']}]で{data['amount']}円使っています。")
-        else:
-            print("その日のデータは見つかりませんでした")
-            continue    
-    while True:
-        money_str=input("金額を入力してください")
-        try:
-            if money=="":
-                print("未入力です")
+            if search_date=="q":
+                break
+
+            if search_date in expences:
+                data=expences[search_date]
+                print(f"【検索結果】{search_date}は[{data['category']}]で{data['amount']}円使っています。\n")
             else:
-                money=int()
-                expences[date]=money
+                print("その日のデータは見つかりませんでした\n")
+                continue 
+        continue   
+    
+    category=input("カテゴリを入力してください(例:食費、交通費):")
+
+    while True:
+        money_str=input("金額を入力してください:")
+        try:
+            if money_str=="":
+                print("未入力です\n")
+            else:
+                money=int(money_str)
+                expences[date]={"amount":money,"category":category}
                 break
         except ValueError:
-            print("半角数字で入力してください")
+            print("半角数字で入力してください\n")
 
+if date!="s":
+    print("\n---家計簿一覧---")
+    total=0
+    for date,data in expences.items():
+        print(f"{date}|カテゴリ:{data['category']}|金額:{data['amount']}円")
+        total+=data['amount']
 
-print("---家計簿一覧---")
-for date,money in expences.items():
-    print(f"{date}:{money}円")
-
-total=sum(expences.values())
-print(f"合計金額:{total}円")
+    print("--------------")
+    print(f"合計金額:{total}円")
