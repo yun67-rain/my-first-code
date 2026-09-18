214423c7b081398dea10e9dd111b13f063deaffa
yun67-rain <yuna.0607.0321@gmail.com>
2026-09-18 05:22:33 +0000

    for

---
+++ java_learning/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<3;i++){
+            System.out.println("HelloWorld!");
+        }
+    }
+}
