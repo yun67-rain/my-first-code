e3f1069076f77965e857e05e3118bce741cbdb7b
yun67-rain <yuna.0607.0321@gmail.com>
2026-09-18 05:28:23 +0000

    while

---
+++ java_learning/HelloWorld3.java
@@ -0,0 +1,9 @@
+public class HelloWorld3 {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<3){
+            System.out.println("HelloWorld!");
+            i++;
+        }
+    }
+}
