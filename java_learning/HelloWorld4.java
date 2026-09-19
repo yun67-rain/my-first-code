3621019efd7b726b759a0ce948d3f90720690b5d
yun67-rain <yuna.0607.0321@gmail.com>
2026-09-19 07:14:21 +0000

    if

---
+++ java_learning/HelloWorld4.java
@@ -0,0 +1,9 @@
+public class HelloWorld4{
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            if(i%2==0){
+                System.out.println(i + "\t" + "HelloWorld!");
+            }
+        }
+    }
+}
